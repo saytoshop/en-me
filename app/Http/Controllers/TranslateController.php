5d5df4b75079cf540f816544a42bd76da85fdc54
saytoshop <2580744@gmail.com>
2022-11-24 13:41:14 +0300

    fix update translation

--- app/Http/Controllers/TranslateController.php
+++ app/Http/Controllers/TranslateController.php
@@ -5,25 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\Translation;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+//use Illuminate\Validation\Validator;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Validator;
+
 
 class TranslateController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-
         return view('addtranslate');
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -34,7 +34,6 @@ class TranslateController extends Controller
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -54,22 +53,18 @@ class TranslateController extends Controller
      * Display the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
         $d = Translation::where('english', $id)->first();
-
         return view('translate', ['english' => $id, 'translates' => $d]);
         //
-
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
      */
     public function edit($id = 0)
     {
@@ -82,12 +77,33 @@ class TranslateController extends Controller
      *
      * @param \Illuminate\Http\Request $request
      * @param int $id
-     * @return \Illuminate\Http\Response
+     *
      */
     public function update(Request $request)
     {
-        dd($request);
-        //
+        $data = [];
+
+        Validator::make($data, [
+            'user' => [
+                'required',
+                Rule::in([auth()->user()->id]),
+            ],
+            'english' => Rule::unique('translations')
+                ->where(fn($query) => $query
+                    ->where('russian', $request['russian'])
+                    ->where('id', '<>', $request['id'])),
+            'russian' => 'required',
+            'example' => 'required'
+        ]);
+        $data = $request->post();
+        unset($data['id']);
+        unset($data['user']);
+        unset($data['_token']);
+        Translation::where('id', $request->id)
+            ->where('user', auth()->user()->id)
+            ->update($data);
+        return redirect()->to('/edit_translate')->with('status', 'Успешно изменена запись');
+//        dd($request);
     }
 
     /**
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -87,16 +87,5 @@
 </nav>
 
 {{$slot}}
-
-
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="/docs/4.6/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
-<script src="/docs/4.6/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
-        crossorigin="anonymous"></script>
-
-
 </body>
 </html>
